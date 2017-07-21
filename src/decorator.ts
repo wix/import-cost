cd6c733b25ed9c8d4935fa7cad89bd1bbdbfb99d
Shahar Talmi <shahart@wix.com>
2017-07-22 01:42:15 +0300

    debouncing decorations

--- src/decorator.ts
+++ src/decorator.ts
@@ -1,6 +1,7 @@
 import { window, Range, Position, DecorationOptions } from 'vscode';
 const DECORATION_COLOR = '#C23B22';
 const decorationsCache = {};
+const decorationsDebounce = {};
 
 export function decorate(text: string, packageInfo) {
   const {fileName, line} = packageInfo;
@@ -10,17 +11,20 @@ export function decorate(text: string, packageInfo) {
       after: { color: DECORATION_COLOR, margin: '0 0 0 1rem' }
     });
   }
-  window.activeTextEditor.setDecorations(
-    decorationsCache[key],
-    <DecorationOptions[]>[
-      {
-        renderOptions: {
-          after: {
-            contentText: text
-          }
-        },
-        range: new Range(new Position(line - 1, 1024), new Position(line - 1, 1024))
-      }
-    ]
-  );
+  clearTimeout(decorationsDebounce[key]);
+  decorationsDebounce[key] = setTimeout(() => {
+    window.activeTextEditor.setDecorations(
+      decorationsCache[key],
+      <DecorationOptions[]>[
+        {
+          renderOptions: {
+            after: {
+              contentText: text
+            }
+          },
+          range: new Range(new Position(line - 1, 1024), new Position(line - 1, 1024))
+        }
+      ]
+    );
+  }, 100);
 }
