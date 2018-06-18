157f20e9ce92f94e7038379025f5a46ea9485754
Timshel Knoll-Miller <timshel@fluentdevelopment.com.au>
2018-06-18 16:52:07 +1000

    Sort JavaScript import specifiers for caching purposes

--- packages/import-cost/src/jsParser.js
+++ packages/import-cost/src/jsParser.js
@@ -57,7 +57,18 @@ function parse(source) {
 function compileImportString(node) {
   let importSpecifiers, importString;
   if (node.specifiers && node.specifiers.length > 0) {
-    importString = node.specifiers
+    importString = [].concat(node.specifiers)
+      .sort((s1, s2) => {
+        // Import specifiers are in statement order, which for mixed imports must be either "defaultImport, * as namespaceImport"
+        // or "defaultImport, { namedImport [as alias]... } according to current ECMA-262.
+        // Given that two equivalent import statements can only differ in the order of the items in a NamedImports block,
+        // we only need to sort these items in relation to each other to normalise the statements for caching purposes.
+        // Where the node is anything other than ImportSpecifier (Babel terminoligy for NamedImports), preserve the original statement order.
+        if (t.isImportSpecifier(s1) && t.isImportSpecifier(s1)) {
+          return s1.imported.name < s2.imported.name ? -1 : 1;
+        }
+        return 0;
+      })
       .map((specifier, i) => {
         if (t.isImportNamespaceSpecifier(specifier)) {
           return `* as ${specifier.local.name}`;
